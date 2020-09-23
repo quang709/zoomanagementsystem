b5fec44ec5f18d856f4fdcdd92dee66ebe51d2f8
quang709 <69231857+quang709@users.noreply.github.com>
2020-09-23 22:39:47 +0700

    zoo management system

---
+++ Tiger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace zoo_management_system
+{
+     class Tiger : Animal
+    {
+
+        public override void Speak()
+        {
+            Console.WriteLine("roarrrrr! ");
+        }
+            public Tiger() : base()
+        {
+
+            }
+        public override void ViewInfo()
+        {
+            base.ViewInfo();
+        }
+        public Tiger(string Name, int Age, string Description) : base(Name, Age, Description)
+        {
+        }
+    }
+}
